720c0a5200bff07326b00792ff77466df2642b8c
MuaazAttari <160422454+MuaazAttari@users.noreply.github.com>
2024-09-01 10:39:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let number = 6
+let remainder = number % 2
+
+console.log(remainder == 0);
+//  It indicates Even Number
+
+console.log(remainder != 0);
+// It indicates Odd Number
---
+++ script.js
@@ -0,0 +1,11 @@
+let number = 5
+let remainder = number % 2
+if(remainder == 0){
+    console.log(`${number} is Even`);
+    
+}
+else{
+    console.log(`${number} is Odd`);
+    
+}
+
